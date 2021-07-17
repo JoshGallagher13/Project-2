87c2dec9eb344e0118b9ce8b2b60e826cbc820cd
JoshGallagher13 <77808673+JoshGallagher13@users.noreply.github.com>
2021-07-17 10:46:34 -0700

    testing changes

---
+++ app.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
+
+#################################################
+# Database Setup
+#################################################
+
+engine = create_engine("sqlite:///Resources/steam.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+sales_data = Base.classes.measurement
+station = Base.classes.station
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def welcome():
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/measurements<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/start date   (YYYY-MM-DD)<br/>"
+        f"/api/v1.0/start date/end date (YYYY-MM-DD)"
