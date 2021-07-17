f032bada565a6e690ed0e343fd2969c23c39ca31
JoshGallagher13 <77808673+JoshGallagher13@users.noreply.github.com>
2021-07-17 14:50:58 -0700

    Added flask, updated data cleaning

--- app.py
+++ app.py
@@ -1,20 +1,28 @@
 import numpy as np
+import pandas as pd
 
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
+from configfile import db_user, db_password, db_host, db_port, db_name
 
 #################################################
 # Database Setup
 #################################################
+### SYNTAX to connect to a database using SQLAlchemy
+# dialect://username:password@databasehost:port/databasename
+###
+# configure the connection string
+rds_connection_string = f'postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}'
+# confirm the connection string
+print(rds_connection_string)
+# connect to the database
+engine = create_engine(rds_connection_string)
+conn = engine.connect()
 
-engine = create_engine("sqlite:///Resources/steam.sqlite")
-Base = automap_base()
-Base.prepare(engine, reflect=True)
-sales_data = Base.classes.measurement
-station = Base.classes.station
+
 
 #################################################
 # Flask Setup
@@ -30,8 +38,40 @@ app = Flask(__name__)
 def welcome():
     return (
         f"Available Routes:<br/>"
-        f"/api/v1.0/measurements<br/>"
-        f"/api/v1.0/stations<br/>"
-        f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/start date   (YYYY-MM-DD)<br/>"
-        f"/api/v1.0/start date/end date (YYYY-MM-DD)"
+        f"/api/v1.0/steam<br/>"
+        
+    )
+
+@app.route("/api/v1.0/steam")
+def steam():
+    df=pd.read_sql("SELECT*from steam",conn)
+    data = df.values.tolist()
+    flask_data=[]
+    for result in range(len(data)):
+        steam_dic = {}
+        steam_dic["Index"] = data[result][0]
+        steam_dic["Game"] = data[result][1]
+        steam_dic["Players"] =data[result][2]
+        steam_dic["Hours"] = data[result][3]
+        steam_dic["Percent_Play"] =data[result][4]
+        steam_dic["Rank"] =data[result][5]
+        steam_dic["Genre"] =data[result][6]
+        steam_dic["ESRB_Rating"] =data[result][7]
+        steam_dic["Platform"] =data[result][8]
+        steam_dic["Publisher"] =data[result][9]
+        steam_dic["Developer"] =data[result][10]
+        steam_dic["Critic_Score"] =data[result][11]
+        steam_dic["User_Score"] =data[result][12]
+        steam_dic["Total_Shipped"] =data[result][13]
+        steam_dic["Global_Sales"] =data[result][14]
+        steam_dic["NA_Sales"] =data[result][15]
+        steam_dic["PAL_Sales"] =data[result][16]
+        steam_dic["JP_Sales"] =data[result][17]
+        steam_dic["Other_Sales"] =data[result][18]
+        steam_dic["Year"] =data[result][19]
+        flask_data.append(steam_dic)
+
+    return jsonify(flask_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
